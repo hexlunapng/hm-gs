4f8a57bcdd889af308352f69199867146cd982f1
Luna <hexishexomgcute@gmail.com>
2025-07-01 14:13:27 +0000

    eh

--- Createprocess.go
+++ Createprocess.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"bufio"
+"bufio"
 	"fmt"
-	"net"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+"net"
 )
 
 
@@ -23,4 +27,113 @@ func main() {
 		}
 		go handleConnection(conn)
 	}
+}
+
+var bLogProcessEvent int32 = 1
+
+
+func rettrue() bool {
+	return true
+}
+
+
+func retfalse() bool {
+	return false
+}
+
+
+func crashaf(a1 int64) bool {
+	fmt.Printf("crash: 0x%x\n", fakeReturnAddress())
+	return false
+}
+
+func crashaf2() bool {
+	fmt.Printf("afwqq23f: 0x%x\n", fakeReturnAddress())
+	return false
+}
+
+func printretaddress() {
+	fmt.Printf("0x%x\n", fakeReturnAddress())
+}
+
+
+func printretaddressaa() {
+	fmt.Printf("wow Print!! 0x%x\n", fakeReturnAddress())
+}
+
+
+func fakeReturnAddress() int64 
+	return 0xDEADBEEF
+}
+
+func FillVendingMachines() {
+	fmt.Println("[Action] FillVendingMachines called")
+}
+
+func inputThread() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Type 'f2' to toggle bLogProcessEvent, 'f3' to fill vending machines: ")
+		if !scanner.Scan() {
+			break
+		}
+		cmd := strings.TrimSpace(scanner.Text())
+		switch strings.ToLower(cmd) {
+		case "f2":
+			newVal := atomic.LoadInt32(&bLogProcessEvent) ^ 1
+			atomic.StoreInt32(&bLogProcessEvent, newVal)
+			fmt.Printf("bLogProcessEvent toggled to %v\n", newVal == 1)
+		case "f3":
+			FillVendingMachines()
+		default:
+			fmt.Println("Unknown command")
+		}
+		time.Sleep(time.Millisecond * 33) 
+	}
+}
+
+func executeConsoleCommand(cmd string) {
+	fmt.Printf("[ConsoleCmd] %s\n", cmd)
+}
+
+
+func main() {
+	fmt.Println("[Init] Base Address: 0xDEADBEEF") 
+	fmt.Println("[Init] UObject::GObjects set")
+
+
+	fmt.Println("[Hook] Created hook at 0x15816C")
+	fmt.Println("[Init] Created DefaultFortGameModeAthena object")
+
+	fmt.Println("[Init] Got PlayerController from World")
+
+	go inputThread()
+
+	
+	commands := []string{
+		"log LogProfileSys VeryVerbose",
+		"log LogFortVolumeManager VeryVerbose",
+		"log LogPlaysetLevelStream VeryVerbose",
+		"log LogFortCustomization VeryVerbose",
+		"log LogFortCosmetics VeryVerbose",
+		"log LogFort VeryVerbose",
+		"log LogFortInventory VeryVerbose",
+		"log LogFortPawnScriptedBehavior VeryVerbose",
+		"log LogFortAI VeryVerbose",
+		"log LogFortLoot VeryVerbose",
+		"log LogFortTeams VeryVerbose",
+		"log LogPlayerPawnAthena VeryVerbose",
+		"log LogOnlineGame VeryVerbose",
+		"log LogAthenaBots VeryVerbose",
+		"log LogNavigation VeryVerbose",
+		"log LogAbilitySystem VeryVerbose",
+		"log LogFortAIDirector VeryVerbose",
+	}
+
+	for _, cmd := range commands {
+		executeConsoleCommand(cmd)
+	}
+
+	
+	select {}
 }
