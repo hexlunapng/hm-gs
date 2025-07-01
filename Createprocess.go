2368172f4f40016d8706be426bf7d85eb9cc3896
Luna <hexishexomgcute@gmail.com>
2025-07-01 14:01:49 +0000

    basic ahh

--- Createprocess.go
+++ Createprocess.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+
+func main() {
+	listener, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+	fmt.Println("Listening on port 7777...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Accept error: %v\n", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
